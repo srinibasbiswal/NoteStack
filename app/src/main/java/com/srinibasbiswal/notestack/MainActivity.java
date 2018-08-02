8fbf34b55ac0bd5af91d94124375e8efd483612c
srinibasbiswal <srinibasbiswal12@gmail.com>
2018-08-02 19:41:19 +0530

    Interlink reg and login activity

--- app/src/main/java/com/srinibasbiswal/notestack/MainActivity.java
+++ app/src/main/java/com/srinibasbiswal/notestack/MainActivity.java
@@ -1,13 +1,60 @@
 package com.srinibasbiswal.notestack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usrEmail, usrPswd;
+    private Button loginBtn;
+    private TextView regLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupUIViews();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()){
+                    // login code
+                }
+            }
+        });
+
+        regLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , RegistrationActivity.class));
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        usrEmail = (EditText)findViewById(R.id.usrEmail);
+        usrPswd = (EditText)findViewById(R.id.usrPswd);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        regLink = (TextView)findViewById(R.id.regLink);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+        String email = usrEmail.getText().toString();
+        String password = usrPswd.getText().toString();
+
+        if (password.isEmpty() && email.isEmpty())
+            Toast.makeText(this,"Please Enter All The Deatils",Toast.LENGTH_SHORT).show();
+        else
+            result  = true;
+
+        return result;
     }
 }
--- app/src/main/java/com/srinibasbiswal/notestack/RegistrationActivity.java
+++ app/src/main/java/com/srinibasbiswal/notestack/RegistrationActivity.java
@@ -1,13 +1,62 @@
 package com.srinibasbiswal.notestack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText usrName, usrEmail, usrPswd;
+    private Button regBtn;
+    private TextView loginLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        setupUIViews();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validate()){
+                    //update on db
+                }
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this , MainActivity.class));
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        usrName = (EditText)findViewById(R.id.usrName);
+        usrEmail = (EditText)findViewById(R.id.usrEmail);
+        usrPswd = (EditText)findViewById(R.id.usrPswd);
+        regBtn = (Button)findViewById(R.id.regBtn);
+        loginLink = (TextView)findViewById(R.id.loginLink);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+        String name = usrName.getText().toString();
+        String email = usrEmail.getText().toString();
+        String password = usrPswd.getText().toString();
+
+        if (name.isEmpty() && password.isEmpty() && email.isEmpty())
+            Toast.makeText(this,"Please Enter All The Deatils",Toast.LENGTH_SHORT).show();
+        else
+            result  = true;
+
+        return result;
     }
 }
