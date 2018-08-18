6159edffa8951c2dbc1c7da4e1564fefef0f98c7
srinibasbiswal <srinibasbiswal12@gmail.com>
2018-08-18 16:48:37 +0530

    Add RecylerView

--- app/src/main/java/com/srinibasbiswal/notestack/Homepage.java
+++ app/src/main/java/com/srinibasbiswal/notestack/Homepage.java
@@ -5,11 +5,25 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import javax.annotation.Nullable;
+
 public class Homepage extends AppCompatActivity {
+
+    private RecyclerView noteList;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,8 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setupUIViews();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +43,27 @@ public class Homepage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        db.collection("notes").document("(CSIT) Computer Science and Information Technology").collection("3rd").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null){
+                    Log.d("Test","Error: "+e.getMessage());
+                }
+                for (DocumentChange doc: queryDocumentSnapshots.getDocumentChanges()){
+                    if(doc.getType() == DocumentChange.Type.ADDED){
+                        String url = doc.getDocument().getString("Download URL");
+                        Log.d("hello","url = "+url);
+                    }
+                }
+            }
+        });
+
+    }
+
+    private void setupUIViews(){
+        noteList = (RecyclerView)findViewById(R.id.noteList);
+        db = FirebaseFirestore.getInstance();
     }
 
 }
---
+++ app/src/main/java/com/srinibasbiswal/notestack/Notes.java
@@ -0,0 +1,4 @@
+package com.srinibasbiswal.notestack;
+
+public class Notes {
+}
